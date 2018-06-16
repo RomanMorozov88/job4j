ece747221e47a89853dac6b75ec640fb3d740b89
RomanMorozov88 <malito:MorozovRoman.88@mail.ru>
2018-06-16 12:50:03 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author Roman Morozov.
+*/
 public class Calculate{
 
+	/**
+	* main.
+	* @param args - args.
+	*/
 	public static void main(String[]args){
 	System.out.println("Hello, World!");
 	}
