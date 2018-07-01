4ab66bec205c73095a861b0673adba7a6f9768c7
RomanMorozov88 <malito:MorozovRoman.88@mail.ru>
2018-07-01 09:45:05 +0300

    6.4. Слова начинается с ...

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenYesterdayTrue() {
+        ArrayChar word = new ArrayChar("Yesterday, all my troubles seemed so far away");
+        boolean result = word.startWith("Yesterday, all my");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenYesterdayWithoutCommaFalse() {
+        ArrayChar word = new ArrayChar("Yesterday all my troubles seemed so far away");
+        boolean result = word.startWith("Yesterday, all my");
+        assertThat(result, is(false));
+    }
+}
