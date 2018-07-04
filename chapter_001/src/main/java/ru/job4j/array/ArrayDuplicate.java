ca60367798a7664f1384db9af7e9a0489c895fb0
RomanMorozov88 <malito:MorozovRoman.88@mail.ru>
2018-07-04 10:28:05 +0300

    6.8. Удаление дубликатов в массиве. Исправлено.

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -10,25 +10,19 @@ public class ArrayDuplicate {
 
         int z = array.length;
 
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i + 1; j < array.length; j++) {
+        for (int i = 0; i < z - 1; i++) {
+            for (int j = i + 1; j < z; j++) {
                 if (array[i].equals(array[j])) {
                     tmp = array[j];
                     // Смещение элементов после дубликата влево.
-                    for (int s = j; s < array.length - 1; s++) {
+                    for (int s = j; s < z - 1; s++) {
                         array[s] = array[s + 1];
                     }
                     // Присвоение последнему элементу значения дубликата.
                     array[array.length - 1] = tmp;
-                }
-            }
-        }
 
-        // Счётчик z для того, что бы знать размер нового массива.
-      for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i].equals(array[j])) {
                     z--;
+                    j--;
                 }
             }
         }
