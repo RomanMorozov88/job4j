b43c5404cc66d06529eb59f406616b226791b301
RomanMorozov88 <malito:MorozovRoman.88@mail.ru>
2018-07-15 14:55:29 +0300

    Получение третьего массива из двух. Подправил.

--- chapter_001/src/main/java/ru/job4j/zadachi/ArrayOfTwo.java
+++ chapter_001/src/main/java/ru/job4j/zadachi/ArrayOfTwo.java
@@ -1,7 +1,7 @@
 package ru.job4j.zadachi;
 
-public class MassivIzDvuh {
-    public int[] sliyanie(int[] arr1, int[] arr2) {
+public class ArrayOfTwo {
+    public int[] MergerArrays(int[] arr1, int[] arr2) {
         int[] result = new int[arr1.length + arr2.length];
 
         int ind1 = 0;
@@ -9,17 +9,13 @@ public class MassivIzDvuh {
 
         for(int i = 0; i < result.length; i++) {
             if(ind1 >= arr1.length){
-                result[i] = arr2[ind2];
-                ind2++;
+                result[i] = arr2[ind2++];
             }else if(ind2 >= arr2.length){
-                result[i] = arr1[ind1];
-                ind1++;
+                result[i] = arr1[ind1++];
             }else if( arr1[ind1]<arr2[ind2]){
-                result[i] = arr1[ind1];
-                ind1++;
+                result[i] = arr1[ind1++];
             }else{
-                result[i] = arr2[ind2];
-                ind2++;
+                result[i] = arr2[ind2++];
             }
         }
         return result;
--- chapter_001/src/test/java/ru/job4j/zadachi/ArrayOfTwoTest.java
+++ chapter_001/src/test/java/ru/job4j/zadachi/ArrayOfTwoTest.java
@@ -4,24 +4,24 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class MassivIzDvuhTest {
+public class ArrayOfTwoTest {
 
     @Test
     public void Arr1PlusArr201() {
-        MassivIzDvuh sln = new MassivIzDvuh();
+        ArrayOfTwo sln = new ArrayOfTwo();
         int[] arr1 = new int[] {3, 8};
         int[] arr2 = new int[] {1, 9};
-        int[] result = sln.sliyanie(arr1, arr2);
+        int[] result = sln.MergerArrays(arr1, arr2);
         int[] expect = {1, 3, 8, 9};
         assertThat(result, is(expect));
     }
 
     @Test
     public void Arr1PlusArr202() {
-        MassivIzDvuh sln = new MassivIzDvuh();
+        ArrayOfTwo sln = new ArrayOfTwo();
         int[] arr1 = new int[] {1, 2, 3};
         int[] arr2 = new int[] {2, 4, 7, 8};
-        int[] result = sln.sliyanie(arr1, arr2);
+        int[] result = sln.MergerArrays(arr1, arr2);
         int[] expect = {1, 2, 2, 3, 4, 7, 8};
         assertThat(result, is(expect));
     }
