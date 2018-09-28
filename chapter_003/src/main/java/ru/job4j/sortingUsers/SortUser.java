bd40f0811a046934468cd7e7ab6f868e81d9d9b8
RomanMorozov88 <malito:MorozovRoman.88@mail.ru>
2018-09-28 17:36:15 +0300

    2. Сортировка User с использованием Comparator.

--- chapter_003/src/main/java/ru/job4j/sortingUsers/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sortingUsers/SortUser.java
@@ -7,4 +7,27 @@ public class SortUser {
     public Set<User> sort (List<User> list) {
         return new TreeSet<>(list);
     }
+
+    public List<User> sortNameLength (List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getName().length(),
+                        o2.getName().length());
+            }
+        });
+        return list;
+    }
+
+    List<User> sortByAllFields (List<User>list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int nameValue = o1.getName().compareTo(o2.getName());
+                int ageValue = Integer.compare(o1.getAge(), o2.getAge());
+                return nameValue != 0 ? nameValue : ageValue;
+            }
+        });
+        return list;
+    }
 }
--- chapter_003/src/test/java/ru/job4j/sortingUsers/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sortingUsers/SortUserTest.java
@@ -19,4 +19,44 @@ public class SortUserTest {
         //Проверяем, что последний элемент в result- это первый элемент во вхожящем листе.
         assertThat(result.last().getName(), is(a1.get(0).getName()));
     }
+
+    @Test
+    public void wheneSortByAllFields() {
+        User u1 = new User("Иван", 25);
+        User u2 = new User("Николай", 22);
+        User u3 = new User("Артём", 20);
+        User u4 = new User("Артём", 25);
+        List<User> a1 = new ArrayList<>();
+        a1.add(u1);
+        a1.add(u2);
+        a1.add(u3);
+        a1.add(u4);
+        List<User> result = new SortUser().sortByAllFields(a1);
+        List<User> expect = new ArrayList<>();
+        expect.add(u3);
+        expect.add(u4);
+        expect.add(u1);
+        expect.add(u2);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void wheneSortNameLength() {
+        User u1 = new User("Иван", 25);
+        User u2 = new User("Николай", 22);
+        User u3 = new User("Артём", 20);
+        User u4 = new User("Артём", 25);
+        List<User> a1 = new ArrayList<>();
+        a1.add(u1);
+        a1.add(u2);
+        a1.add(u3);
+        a1.add(u4);
+        List<User> result = new SortUser().sortNameLength(a1);
+        List<User> expect = new ArrayList<>();
+        expect.add(u1);
+        expect.add(u3);
+        expect.add(u4);
+        expect.add(u2);
+        assertThat(result, is(expect));
+    }
 }
