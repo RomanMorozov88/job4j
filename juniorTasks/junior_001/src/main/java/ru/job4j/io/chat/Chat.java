2c0cbb6b1bca6b25640f95e43bdfa7b7bd9cb52d
RomanMorozov88 <MorozovRoman.88@mail.ru>
2019-03-10 11:45:18 +0300

    Создать программу 'Консольный чат'.

---
+++ juniorTasks/junior_001/src/main/java/ru/job4j/io/chat/Chat.java
@@ -0,0 +1,117 @@
+package ru.job4j.io.chat;
+
+import java.io.*;
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Chat {
+
+    private String stopWord = "стоп";
+    private String continueWord = "продолжить";
+    private String exitWord = "закончить";
+    private boolean switchValue = true;
+    private Consumer textingOut = System.out::println;
+
+    private File answerFile =
+            new File("juniorTasks\\junior_001\\src\\main\\java\\ru\\job4j\\io\\chat\\answers.txt");
+    private File log =
+            new File("juniorTasks\\junior_001\\src\\main\\java\\ru\\job4j\\io\\chat\\log.txt");
+
+    public Chat() {
+    }
+
+    /**
+     * Коснтруктор для теста.
+     */
+    public Chat(File newLog, File newAnswers) {
+        this.answerFile = newAnswers;
+        this.log = newLog;
+    }
+
+    /**
+     * Метод, в котором находится цикл с чатом, запись вопросов-ответов,
+     * проверка условий молчания\выхода.
+     *
+     * @param
+     * @throws IOException
+     */
+    public void mainMethod(InputStream in) throws IOException {
+
+        try (in;
+             FileWriter logWriter = new FileWriter(this.log, true);
+             RandomAccessFile raf = new RandomAccessFile(this.answerFile, "r")
+        ) {
+            Scanner scanner = new Scanner(in);
+            List<Integer> positionsOfDots = this.setIndexOfDots();
+            String inputString = scanner.nextLine();
+
+            while (!inputString.equals(this.exitWord)) {
+                this.switcher(inputString);
+                logWriter.write("-INPUTPHRASE: " + inputString + System.lineSeparator());
+                if (this.switchValue) {
+                    raf.seek(this.randomPoint(positionsOfDots));
+                    String answer = raf.readLine();
+                    answer = answer.substring(0, answer.indexOf('.') + 1);
+                    logWriter.write("-ANSWERPHRASE: " + answer + System.lineSeparator());
+                    this.textingOut.accept(answer);
+                }
+                inputString = scanner.nextLine();
+            }
+            logWriter.write("-ERXITPHRASE: " + inputString + System.lineSeparator());
+        }
+    }
+
+    /**
+     * Проверка входящего сообщения словам stopWord или continueWord.
+     * Меняем switchValue.
+     *
+     * @param string
+     */
+    private void switcher(String string) {
+        if (string.equals(this.stopWord)) {
+            this.switchValue = false;
+        } else if (string.equals(this.continueWord)) {
+            this.switchValue = true;
+        }
+    }
+
+    /**
+     * Метод для получения случайного индекса для метода .seek
+     *
+     * @return
+     * @throws IOException
+     */
+    private int randomPoint(List<Integer> dots) {
+        int max = dots.size();
+        int index = (int) (Math.random() * max);
+        return dots.get(index);
+    }
+
+    /**
+     * Смотрим, на каких позициях находятся точки и записываем позиции первых букв слов,
+     * находящихся после точек.
+     * Так же добавляем нулевую позицию.
+     * Выкидываем позицию самой последней точки.
+     * Не учитываем пунктуационные изыски вроде многоточия, восклицательного, вопросительного знаков.
+     * Потому возможны ответы в виде пустой строки.
+     *
+     * @return
+     * @throws IOException
+     */
+    private List<Integer> setIndexOfDots() throws IOException {
+        List<Integer> indexs = new ArrayList<>();
+        FileInputStream inAnswer = new FileInputStream(this.answerFile);
+        byte[] array = new byte[inAnswer.available()];
+        inAnswer.read(array);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == '.') {
+                indexs.add(i + 2);
+            }
+        }
+        if (indexs.size() > 1) {
+            indexs.remove(indexs.size() - 1);
+        }
+        indexs.add(0);
+        return indexs;
+    }
+}
---
+++ juniorTasks/junior_001/src/test/java/ru/job4j/io/chat/ChatTest.java
@@ -0,0 +1,83 @@
+package ru.job4j.io.chat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ChatTest {
+
+    String path = System.getProperty("java.io.tmpdir");
+
+    Chat chat;
+    FileWriter testAnswerWriter;
+    BufferedReader logReader;
+    String ansPath;
+    String logPath;
+    File testAnsw;
+    File testLog;
+
+    /**
+     * Метод для очистки testLog.
+     * @param logFile
+     * @throws IOException
+     */
+    private void clearLog(File logFile) throws IOException {
+        if (logFile.exists()) {
+            logFile.delete();
+            logFile.createNewFile();
+        }
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        ansPath = path + "\\testAnswers";
+        logPath = path + "\\testLog";
+
+        testAnsw = new File(ansPath);
+        testAnsw.createNewFile();
+        testLog = new File(logPath);
+        clearLog(testLog);
+
+        testAnswerWriter = new FileWriter(testAnsw);
+        testAnswerWriter.write("Test. Test. Test. Test.");
+        testAnswerWriter.close();
+
+        logReader = new BufferedReader(new FileReader(testLog));
+        chat = new Chat(testLog, testAnsw);
+    }
+
+    @Test
+    public void chatRun() throws IOException {
+        String inputString = "старт" + System.lineSeparator() +
+                "раз" + System.lineSeparator() +
+                "стоп" + System.lineSeparator() +
+                "два" + System.lineSeparator() +
+                "продолжить" + System.lineSeparator() +
+                "три" + System.lineSeparator() +
+                "закончить";
+        ByteArrayInputStream bIn = new ByteArrayInputStream(inputString.getBytes());
+        chat.mainMethod(bIn);
+
+        String logResult = "";
+        String buffer;
+        while ((buffer = logReader.readLine()) != null) {
+            logResult += buffer + System.lineSeparator();
+        }
+        String expected = "-INPUTPHRASE: старт" + System.lineSeparator() +
+                "-ANSWERPHRASE: Test." + System.lineSeparator() +
+                "-INPUTPHRASE: раз" + System.lineSeparator() +
+                "-ANSWERPHRASE: Test." + System.lineSeparator() +
+                "-INPUTPHRASE: стоп" + System.lineSeparator() +
+                "-INPUTPHRASE: два" + System.lineSeparator() +
+                "-INPUTPHRASE: продолжить" + System.lineSeparator() +
+                "-ANSWERPHRASE: Test." + System.lineSeparator() +
+                "-INPUTPHRASE: три" + System.lineSeparator() +
+                "-ANSWERPHRASE: Test." + System.lineSeparator() +
+                "-ERXITPHRASE: закончить" + System.lineSeparator();
+        assertThat(logResult, is(expected));
+    }
+}
