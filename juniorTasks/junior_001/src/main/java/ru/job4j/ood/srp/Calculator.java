96810ab620db208617c7874d18e981fbff7cb2fa
RomanMorozov88 <MorozovRoman.88@mail.ru>
2019-05-01 12:59:00 +0300

    1. Использую класса Calculator. Сделать класс InteractCalc.

---
+++ juniorTasks/junior_001/src/main/java/ru/job4j/ood/srp/Calculator.java
@@ -0,0 +1,20 @@
+package ru.job4j.ood.srp;
+
+public class Calculator {
+
+    public static double add(double first, double second) {
+        return first + second;
+    }
+
+    public static double subtract(double first, double second) {
+        return first - second;
+    }
+
+    public static double div(double first, double second) {
+        return first / second;
+    }
+
+    public static double multiple(double first, double second) {
+        return first * second;
+    }
+}
